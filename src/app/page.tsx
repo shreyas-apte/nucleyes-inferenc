74bec7a53fd27e2646241b99132e0b49da956411
Shreyas Apte <shreyas.s.apte@gmail.com>
2025-04-05 09:49:00 +0000

    Bypassed Auth for Testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 
-import { auth } from "./auth";
+// import { auth } from "./auth";
 import DefaultDashboard from "./default-dashboard";
 
 export default async function Home() {
-  const session = await auth();
-  console.log({ session });
-
-  if (!session) {
-    redirect("/login");
-  }
-
+  // const session = await auth();
+  // console.log({ session });
+  // if (!session) {
+  //   redirect("/login");
+  // }
   return <DefaultDashboard />;
 }
